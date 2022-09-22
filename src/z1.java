0ce68705c79f4d8bcccc8c29578685b31b167998
arseso <yoda.master.pro@gmail.com>
2022-09-22 21:52:26 +0300

    add

--- src/z1.java
+++ src/z1.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 
 public class z1 {
     public static void main(String[] args) {
-        System.out.print("Введите сумму цифр трёхзначного числа: ");
-        Scanner console = new Scanner(System.in);
-        int a=0;
-        if(!console.hasNextInt() || (a=console.nextInt())<0 || a>=28){
-            System.out.println("Указан неверный параметр.");
-            return;
-        }
+
+        int a=input();
+        if(a == 0) return;
+        printresult(a);
+
+    }
+
+    private static void printresult(int a) {
         System.out.println("Полученные числа:");
         for (int i = 100; i < 1000; i++) {
             int sum = (i % 10) + ((i / 10) % 10) + ((i / 100) % 10);
@@ -17,4 +18,16 @@ public class z1 {
             }
         }
     }
+
+    private static int input() {
+        int a = 0;
+        System.out.print("Введите сумму цифр трёхзначного числа: ");
+        Scanner console = new Scanner(System.in);
+        if(!console.hasNextInt() || (a=console.nextInt())<0 || a>=28){
+            System.out.println("Указан неверный параметр.");
+            return 0;
+        }
+        return a;
+    }
+
 }
